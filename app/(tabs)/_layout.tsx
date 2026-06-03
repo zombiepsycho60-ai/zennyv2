0cf9949d6b1011e49421aecdd05fe74abc8ac30e
zombiepsycho60-ai <zombiepsycho60@gmail.com>
2026-06-03 06:22:44 +0530

    add app/(tabs)/_layout.tsx

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,15 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { display: "none" },
+      }}
+    >
+      <Tabs.Screen name="index" />
+    </Tabs>
+  );
+}
